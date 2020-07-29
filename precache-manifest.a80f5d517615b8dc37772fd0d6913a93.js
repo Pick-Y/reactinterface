134d8f69f5538f28d22810295018a152ed69c8f7
Alessndro Scarcella <Alessandroscarcella@Alessndros-MBP.lan>
2020-07-29 13:01:30 +1000

    Updates

---
+++ precache-manifest.a80f5d517615b8dc37772fd0d6913a93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596f129374604e679b761df1a0071c0e",
+    "url": "/reactinterface.git/index.html"
+  },
+  {
+    "revision": "2c09bebe1971d06c8c60",
+    "url": "/reactinterface.git/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4d9fffdd70998fa8c9c8",
+    "url": "/reactinterface.git/static/css/main.1ceb1bce.chunk.css"
+  },
+  {
+    "revision": "2c09bebe1971d06c8c60",
+    "url": "/reactinterface.git/static/js/2.5fe81ee6.chunk.js"
+  },
+  {
+    "revision": "fea4e98622268b2f0cf3f0df55dd1915",
+    "url": "/reactinterface.git/static/js/2.5fe81ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9fffdd70998fa8c9c8",
+    "url": "/reactinterface.git/static/js/main.ee9311b8.chunk.js"
+  },
+  {
+    "revision": "aff36e05099dfcc91fce",
+    "url": "/reactinterface.git/static/js/runtime-main.424fee1a.js"
+  }
+]);
